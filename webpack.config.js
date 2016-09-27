9db1bcc0e0df6c910199814b26c07ebaff74c9b4
David Hill <david@davidhill.ie>
2016-09-27 18:22:09 +0100

    Covert convig to function

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-var path = require('path');
+var path = require('path'),
+    resolve = path.resolve;
 
-module.exports = {
-  entry: './src/js/app.js',
-  output: {
-    path: path.resolve(__dirname, 'webp'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['es2015', { 'modules': false }]
-          ]
+module.exports = env => {
+  return {
+    entry: './src/js/app.js',
+    output: {
+      path: resolve(__dirname, 'webp'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [
+            resolve(__dirname, 'src')
+          ],
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['es2015', { 'modules': false }]
+            ]
+          }
+
         }
-
-      }
-    ]
+      ]
+    }
   }
 }
