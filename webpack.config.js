8170d0682eb28fa6f26b9ef9586da22491da3015
David Hill <david@davidhill.ie>
2016-09-28 18:35:21 +0100

    Add output optimisation plugins to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,68 @@
+/* global require, module, __dirname */
+
 var path = require('path'),
+    webpack = require('webpack'),
     resolve = path.resolve;
 
-module.exports = env => {
-  return {
-    entry: './src/js/app.js',
-    output: {
-      path: resolve(__dirname, 'webp'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          include: [
-            resolve(__dirname, 'src')
-          ],
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['es2015', { 'modules': false }]
-            ]
-          }
 
-        }
-      ]
-    }
-  }
-}
+module.exports = env => {
+
+    var addPlugin = (add, plugin) =>{
+        add ? plugin : undefined;
+    };
+
+    var ifProd = plugin => {
+        addPlugin(env.prod, plugin);
+    };
+
+    var removeEmpty = i => {
+        i !== undefined;
+    };
+
+    return {
+        devtool: 'source-map',
+        entry: './src/js/app.js',
+        output: {
+            path: resolve(__dirname, 'webp'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    include: [
+                        resolve(__dirname, 'src')
+                    ],
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                              ['es2015', { 'modules': false }]
+                        ]
+                    }
+
+                }
+            ]
+        },
+        plugins: [
+            ifProd( new webpack.optimize.DedupePlugin() ),
+            ifProd( new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false
+            }),
+            ifProd( new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: 'production'
+                }
+            })),
+            ifProd( new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    screw_ie8: true,
+                    warnings: true,
+                    sourceMap: true
+                }
+            }))
+           )
+        ].filter(removeEmpty)
+    };
+};
