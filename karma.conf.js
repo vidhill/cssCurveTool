2f0389e4357250db6b17cbeae10138d086e81509
David Hill <david@davidhill.ie>
2016-09-29 22:09:04 +0100

    Swap out Mocha and Chai for Jasmine

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-/* global module, require  */
+/* eslint-env node */
 
 var testGlob = 'src/js/**.spec.js',
     srcGlob = 'src/js/**!(test).js',
@@ -8,7 +8,7 @@ var testGlob = 'src/js/**.spec.js',
 module.exports = function(config) {
     config.set({
         basePath: '',
-        frameworks: ['mocha', 'chai'],
+        frameworks: ['jasmine'],
         files: [
             'node_modules/snapsvg/dist/snap.svg.js',
             testGlob,
@@ -25,7 +25,15 @@ module.exports = function(config) {
         webpackMiddleware: {
             noInfo: true // disable webpack build output for tests
         },
-        reporters: ['progress', 'coverage'],
+        reporters: ['spec', 'coverage', 'html'],
+        htmlReporter: {
+            outputDir: 'karma_html', // where to put the reports
+            focusOnFailures: true, // reports show failures on start
+            namedFiles: false, // name files instead of creating sub-directories
+            pageTitle: null, // page title for reports; browser info by default
+            urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+            reportName: 'report-summary-filename', // report summary filename; browser info by default
+        },
         coverageReporter: {
             reporters: [
                 { type: 'lcov', dir:'cover', subdir: '.' },
