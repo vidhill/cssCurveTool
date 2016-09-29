ac51e20bc4e61892bc771d9ebc8983283d444db9
David Hill <david@davidhill.ie>
2016-09-29 22:33:08 +0100

    Lint fixes

--- src/js/helpers.spec.js
+++ src/js/helpers.spec.js
@@ -3,13 +3,13 @@
 import helpers from './helpers';
 
 describe('The \'isObject\' method', () => {
-    it('should return true for an empty object', ()=>{
+    it('should return true for an empty object', () => {
         expect(helpers.isObject({})).toBe(true);
     });
-    it('should return false for undefined', ()=>{
+    it('should return false for undefined', () => {
         expect(helpers.isObject(undefined)).toBe(false);
     });
-    it('works as well', ()=>{
+    it('works as well', () => {
         expect(helpers.isObject).not.toBeUndefined();
     });
 });
